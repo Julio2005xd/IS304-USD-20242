40e1ff440551ce92fcbdbd307d5225ddf0a0e4ac
Julio2005xd <jchernanmon@unimonserrate.edu.co>
2024-08-31 23:22:22 -0500

    Programa version 0.2

--- SESION03/tarea1.py
+++ SESION03/tarea1.py
@@ -7,6 +7,30 @@ Crear un menú para crear objetos y realizar las diversas operaciones referidas.
 
 '''
 class CuentaBancaria:
-    def __numeroCta__(self, NumeroCuenta):
-        self__NumeroCuenta = NumeroCuenta
+    __numeroCta = 0
+    __nombreCliente = ""
+    __fechaApertura = "00/00/0000"
+    __saldo = 0
+    def __init__(self, __numeroCta, __nombreCliente, __fechaApertura, __saldo) -> None:
+        self.__numeroCta = __numeroCta
+        self.__nombreCliente = __nombreCliente
+        self.__fechaApertura = __fechaApertura
+        self.__saldo = __saldo
     
+    def mostrar_info(self):
+        print(f"Número de cuenta: {self.__numeroCta}")
+        print(f"Titular: {self.__nombreCliente}")
+        print(f"Saldo: {self.__saldo}")
+    
+class Menu:
+    def __init__(self):
+        self.cuentas = []
+    
+    def aperturar_Cuenta(self, numero_cuenta, titular, fechaApertura = 0, saldo_inicial=0):
+        nueva_cuenta = CuentaBancaria(numero_cuenta, titular, fechaApertura, saldo_inicial)
+        self.cuentas.append(nueva_cuenta)
+        print(f"Cuenta para {titular} con fecha de creacion de cuenta {fechaApertura} y numero de cuenta {numero_cuenta} ha sido aperturada con éxito.")
+
+mi_banco = Menu()
+mi_banco.aperturar_Cuenta("123456789", "Juan Pérez", "2/3/2024", 2000)
+

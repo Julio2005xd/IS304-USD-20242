353721bc60106680d134db4be1034df51050e63d
Julio2005xd <159793096+Julio2005xd@users.noreply.github.com>
2024-09-18 07:55:48 -0500

    PARCIAL1 FINAL

--- PARCIAL1/parcial1.py
+++ PARCIAL1/parcial1.py
@@ -23,6 +23,12 @@ class NumerosPrimos:
     def obtener_primos(self):
         return self.__obtener_primos_hasta(self.__numero)
 
+    def get_numero(self):
+        return self.__numero
+
+    def set_numero(self, numero):
+        self.__numero = numero
+
 class main:
     print("Por favor digite un numero")
     numero = int(input())
